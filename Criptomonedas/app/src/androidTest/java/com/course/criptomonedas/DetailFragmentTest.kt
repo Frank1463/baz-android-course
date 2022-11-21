80eac32236928bdac0573f1c778aaef6f43c6b11
Francisco Garcia <francisco.garciago@elektra.com>
2022-11-21 01:07:11 -0600

    3er Entregable -Linter implementation -tests

--- Criptomonedas/app/src/androidTest/java/com/course/criptomonedas/DetailFragmentTest.kt
+++ Criptomonedas/app/src/androidTest/java/com/course/criptomonedas/DetailFragmentTest.kt
@@ -10,9 +10,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-
 
 @RunWith(AndroidJUnit4::class)
 class DetailFragmentTest {
